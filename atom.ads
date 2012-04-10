93606d0034b64b5e828d57376d16d2d4072097ab
Felix Stridsberg <felixstridsberg@gmail.com>
2012-04-10 16:23:28 +0200

    Atom

---
+++ atom.ads
@@ -0,0 +1,27 @@
+
+package Atom is
+   type Atom_Ptr is private;
+   type Atom_Type is private;
+
+   function Create_Atom(X, Y, Z : Integer) return Atom_Ptr;
+   
+   function Get_X(Atom : in Atom_Ptr) return Integer;
+   function Get_Y(Atom : in Atom_Ptr) return Integer;
+   function Get_Z(Atom : in Atom_Ptr) return Integer;
+   
+   procedure Set_X(Atom : in Atom_Ptr; X : in Integer);
+   procedure Set_Y(Atom : in Atom_Ptr; Y : in Integer);
+   procedure Set_Z(Atom : in Atom_Ptr; Z : in Integer);
+   
+   procedure Free_Atom(Atom : in out Atom_Ptr);
+
+private
+   type Atom_Ptr is access Atom_Type;
+   type Atom_Type is
+	  record
+		 X : Integer;
+		 Y : Integer;
+		 Z : Integer;
+	  end record;
+
+end Atom;
