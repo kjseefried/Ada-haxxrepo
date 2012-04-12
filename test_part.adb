f9784a7c4803b88eea7957a8f511a9ec5a12012d
Micke <micma567@student.liu.se>
2012-04-12 17:44:43 +0200

    +contains +Min

--- test_part.adb
+++ test_part.adb
@@ -13,6 +13,7 @@ procedure Test_Part is
     Atom3,
     Atom4,
     Atom5,
+    Atom6,
 	Orig_Atom: Atom_Ptr;
 
 begin
@@ -25,6 +26,7 @@ begin
     Atom3 := Create_Atom(3,-1,0);
     Atom4 := Create_Atom(1,-2,0);
     Atom5 := Create_Atom(1,-3,0);
+    Atom6 := Create_Atom(1,-3,0);
 
     Insert(Part, Atom1);
     Insert(Part, Atom2);
@@ -32,9 +34,9 @@ begin
     --Insert(Part, Atom4);
     --Insert(Part, Atom5);
 
-	
+
     Put(Part);
-	
+
     New_Line(3);
     Rotate_z(Part);
     Put(Part);
@@ -50,7 +52,7 @@ begin
 	else
 		Put_Line("Test error! Atom1 stämmer inte med original-atom");
 	end if;
-	
+
     Free(Atom1);
     Free(Atom2);
     Free(Atom3);
@@ -59,10 +61,10 @@ begin
 	Free(Part);
 
 	Put_Line("-----END ROTATION-----");
-	
+
 	New_Line(2);
 	--Förflyttning--
-	
+
 	Put_Line("------FÖRFLYTTNING-------");
     Part := Create_Part;
     Atom1 := Create_Atom(0,0,0);
@@ -75,34 +77,45 @@ begin
     Insert(Part, Atom3);
     Insert(Part, Atom4);
     Insert(Part, Atom5);
-	
+
 	Put(Part);
-	
+
 	Put_Line("Flytta +1 X, +2 Y, +3 Z");
 	Move_X(Part, 1);
 	Move_Y(Part, 2);
 	Move_Z(Part, 3);
     Put(Part);
-	
+
 	Put_Line("Flytta -1 X, -2 Y, -3 Z");
 	Move_X(Part, -1);
 	Move_Y(Part, -2);
 	Move_Z(Part, -3);
     Put(Part);
-	
+
+
+	Put_Line("------END FÖRFLYTTNING-------");
+    new_line(2);
+
+
+    put_line("-----------Contains------");
+
+    if contains(part,atom1) then
+        put_line("fuck yea");
+   end if;
+
+    if contains(part,atom6) then
+        put_line("fuck yea2");
+    else
+        put_line("nope");
+    end if;
+    put("----end contains----");
+
     Free(Atom1);
     Free(Atom2);
     Free(Atom3);
 	Free(Atom4);
 	Free(Atom5);
-	Free(Part);	
-
-	Put_Line("------END FÖRFLYTTNING-------");
-	
-
-	
-
-	
+	Free(Part);
 
 end Test_Part;
 
